bd169cc7d6902970e4cc67f4ba9d86602ae80fd1
Ken Gardiner <ken_gardiner@yahoo.com>
2016-08-25 11:54:36 -0400

    Initial commit

---
+++ app/src/main/java/com/cybermoosemoosemedia/www/simpletodo/MainActivity.java
@@ -0,0 +1,64 @@
+package com.cybermoosemoosemedia.www.simpletodo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
+
+
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ADD HERE
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+        // Setup remove listener method call
+        setupListViewListener();
+
+    }
+
+    // Add Items to list
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+    // Delete items from the list on long click
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+                });
+    }
+}
+
